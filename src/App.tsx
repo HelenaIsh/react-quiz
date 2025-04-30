3eaed2fadf3eaaaf7faf4ec3bcedace3ce290204
HelenaIsh <elena-ishmatova@mail.ru>
2025-04-30 12:43:24 +0200

    feat: implement starter components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import DateCounter from './DateCounter.tsx';
+import Error from './Error.tsx';
+import Header from './Header.tsx';
+import Loader from './Loader.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <DateCounter />
+      <Error />
+      <Loader />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
